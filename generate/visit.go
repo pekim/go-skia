7a6ef8ba8045ad642903fdb31a4713a4ad2d16aa
Mike D Pilsbury <mike.pilsbury@gmail.com>
2024-09-02 08:15:43 +0100

    Add dumpCursor function

--- generate/visit.go
+++ generate/visit.go
@@ -1,6 +1,8 @@
 package generate
 
 import (
+	"fmt"
+
 	"github.com/go-clang/clang-v15/clang"
 )
 
@@ -8,12 +10,14 @@ func (g *generator) visit() {
 	g.transUnit.TranslationUnitCursor().Visit(func(cursor, parent clang.Cursor) (status clang.ChildVisitResult) {
 		switch cursor.Kind() {
 		case clang.Cursor_ClassDecl:
+			// dumpCursor(cursor, "")
 			class := newClass(cursor)
 			if class.isPublic {
 				g.classes = append(g.classes, class)
 			}
 
 		case clang.Cursor_EnumDecl:
+			// dumpCursor(cursor, "")
 			if !cursor.IsAnonymous() {
 				g.enums = append(g.enums, newEnum(cursor, nil))
 			}
@@ -21,4 +25,42 @@ func (g *generator) visit() {
 
 		return clang.ChildVisit_Continue
 	})
+}
+
+type speller interface {
+	Spelling() string
+}
+
+const dumpIndent = "  "
+
+func dumpCursor(cursor clang.Cursor, prefix string) {
+	fmt.Printf("%s%s : %s : %s\n", prefix,
+		cursor.Kind().Spelling(),
+		cursor.Spelling(),
+		cursor.Type().Kind())
+
+	dumpSpelling(prefix, "Type", cursor.Type())
+	if cursor.AccessSpecifier() != clang.AccessSpecifier_Invalid {
+		dumpSpelling(prefix, "AccessSpecifier", cursor.AccessSpecifier())
+	}
+	dumpCursorValue(prefix, "DisplayName", cursor.DisplayName())
+	if cursor.CXXRecord_IsAbstract() {
+		fmt.Println(prefix, "Abstract")
+	}
+
+	cursor.Visit(func(cursor, _ clang.Cursor) (status clang.ChildVisitResult) {
+		dumpCursor(cursor, prefix+dumpIndent)
+		return clang.ChildVisit_Continue
+	})
+}
+
+func dumpCursorValue(prefix string, name string, value string) {
+	if len(value) > 0 {
+		fmt.Printf("%s%s = %s\n", prefix+dumpIndent+dumpIndent, name, value)
+	}
+}
+
+func dumpSpelling(prefix string, name string, speller speller) {
+	value := speller.Spelling()
+	dumpCursorValue(prefix, name, value)
 }
